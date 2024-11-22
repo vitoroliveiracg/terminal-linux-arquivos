b15c5bd6a29fdfe5d2e3a4bd05c81445dbdf0d38
Vitor Oliveira <vitor.oliveira.1@aluno.cefet-rj.br>
2024-11-22 16:02:12 -0300

    Exercicio Feito

--- trabalho.sh
+++ trabalho.sh
@@ -1,1 +1,36 @@
 #!/bin/bash
+
+# 1
+sudo apt install -y fortune 
+
+#2
+fortune > mensagem1.txt
+fortune > mensagem2.txt
+fortune > mensagem3.txt
+
+#3
+cat mensagem1.txt mensagem2.txt mensagem3.txt > mensagens.txt
+
+#4
+cp mensagens.txt mensagens_antiga.txt
+
+fortune >> mensagens.txt
+
+#5
+wc -l mensagens.txt | cut -d " " -f 1 > linhas.txt
+
+#6
+grep -Fo "a" mensagens.txt | wc -l > letras.txt
+
+#7
+ls -R /etc 1> saida.txt 2> erros.txt
+
+#8
+ls -R /etc > saida_com_erros.txt 2>&1
+
+#9
+cat /etc/passwd | cut -d ":" -f 1 > usuarios.txt
+
+#10
+cat /etc/passwd | cut -d ":" -f 1 | sort > usuarios_ordenados.txt
+
